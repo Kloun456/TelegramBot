d11fd383563a7f978a4fca75fa47451ab11297eb
Egor <78965361+Kloun456@users.noreply.github.com>
2024-03-08 13:28:38 +0300

    feat: инициализация проекта

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using SKitLs.Bots.Telegram.Core.Model.Building;
+using SKitLs.Bots.Telegram.Core.Model.Interactions.Defaults;
+using SKitLs.Bots.Telegram.Core.Model.Management.Defaults;
+using SKitLs.Bots.Telegram.Core.Model.UpdateHandlers.Defaults;
+using SKitLs.Bots.Telegram.Core.Model.UpdatesCasting.Signed;
+
+namespace TelegramBotExperiments
+{
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var privateMessages = new DefaultSignedMessageUpdateHandler();
+            var privateTexts = new DefaultSignedMessageTextUpdateHandler
+            {
+                CommandsManager = new DefaultActionManager<SignedMessageTextUpdate>()
+            };
+            privateTexts.CommandsManager.AddSafely(StartCommand);
+            privateMessages.TextMessageUpdateHandler = privateTexts;
+
+            ChatDesigner privates = ChatDesigner.NewDesigner()
+               .UseMessageHandler(privateMessages);
+
+            await BotBuilder.NewBuilder("6435771255:AAHigtm3iMfduaIBEJMRkZqlwzlL5Ndcp8o")
+               .EnablePrivates(privates)
+               .Build()
+               .Listen();
+        }
+
+        private static DefaultCommand StartCommand => new("start", Do_StartAsync);
+        private static async Task Do_StartAsync(SignedMessageTextUpdate update)
+        {
+            await update.Owner.DeliveryService.AnswerSenderAsync("Hello, world!", update);
+        }
+    }
+}
